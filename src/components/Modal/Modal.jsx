c70d53eecda587b7b679a6d18d3a51b2669161c6
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-07-08 23:12:43 +0200

    Création de la modal pour afficher les travaux dynamiquement

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import './modal.css';
 
-const ProjectDetail = () => {
+const Modal = ({ project, onClose }) => {
   return (
-    <div>ProjectDetail</div>
-  )
-}
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button className="close-button" onClick={onClose}>&times;</button>
+        <h2>{project.title}</h2>
+        <p>{project.details}</p>
+        <div className="project-images">
+          {project.images.map((image, index) => (
+            <img key={index} src={image} alt={`${project.title} ${index + 1}`} />
+          ))}
+        </div>
+        <div className="technologies">
+          <h3>Technologies utilisées :</h3>
+          <ul>
+            {project.technologies.map((tech, index) => (
+              <li key={index}>{tech}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="links">
+          <a href={project.githubLink} target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href={project.demoLink} target="_blank" rel="noopener noreferrer">Demo</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProjectDetail
+export default Modal;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react";
 import projectsData from "../../data/projects.json";
 import './project.css';
+import Modal from "../Modal/Modal";
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
+  const [selectedProject, setSelectedProject] = useState(null)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   useEffect(() => {
     setProjects(projectsData);
   }, []);
+
+  const openModal = (project) =>{
+    setSelectedProject(project)
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () =>{
+    setIsModalOpen(false)
+    setSelectedProject(null)
+  }
 
   return (
     <section id="projects">
@@ -20,9 +33,13 @@ const Projects = () => {
             <img src={project.coverPicture} alt={`${project.title} cover`} />
             <h3>{project.title}</h3>
             <p>{project.description}</p>
+            <button className="see-more-btn" onClick={() => openModal(project)}>Voir plus</button>
           </div>
         ))}
       </div>
+      {isModalOpen && (
+        <Modal project={selectedProject} onClose={closeModal}/>
+      )}
     </section>
   );
 };
